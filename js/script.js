0d0eda7221d9004c594aa123eaaf5afd4c553432
Victor <victor.faskin@gmail.com>
2019-04-04 10:37:45 +0500

    check for doublet

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const sizeX = 3;
 const sizeY = 4;
 const gridSize = sizeX * sizeY;
-const timeRembemer = 1000;
-
+const timeForRembemer = 1000;
+const timeBeforeUnflip = 800;
 
 allCards.sort(() => Math.random() - 0.5); // Перемешиваем все карты
 
@@ -15,26 +15,17 @@ for (i = 0; i < gridSize / 2; i++) { // Массив используемых к
 
 cards.sort(() => Math.random() - 0.5); // Перемешиваем используемые карты вместе с парами
 
-
-function preloadImage(url)
-{
-    var img = new Image();
-    img.src = url;
-}
-
-preloadImage('img/cardBack.png');
-
 function generateGrid() {
     let grid = document.getElementById('grid');
-    for (let i = 0; i < gridSize; i ++) {
+    for (let i = 0; i < gridSize; i++) {
         let griddiv = document.createElement('div');
         griddiv.setAttribute('class', 'game-card flip');
+        griddiv.setAttribute('data-id', cards[i].id);
         grid.appendChild(griddiv);
 
         let griddivImgfront = document.createElement('img');
         griddivImgfront.setAttribute('src', cards[i].img);
         griddivImgfront.setAttribute('class', 'front-card');
-        griddivImgfront.setAttribute('data-id', cards[i].id);
         griddiv.appendChild(griddivImgfront);
 
         let griddivImgback = document.createElement('img');
@@ -42,7 +33,6 @@ function generateGrid() {
         griddivImgback.setAttribute('class', 'back-card');
         griddiv.appendChild(griddivImgback);
 
-        preloadImage(cards[i].img);
     }         
 };
 
@@ -63,24 +53,78 @@ function updateSizeGrid(){
 };
 
 
-
 generateGrid();
 updateSizeGrid();
 
 $(window).resize(function(){
     updateSizeGrid();
 });
+       
 
-            
-const divCards = document.querySelectorAll('.game-card');
+const htmlCards = document.querySelectorAll('.game-card');
+
+let flippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    if (lockCards) {return;}
+    if (this === firstCard) {return;}
+
+    this.classList.add('flip');
+
+    if (!flippedCard) {
+        flippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    lockCards = true;
+
+    checkForDoublet();
+}
+
+function checkForDoublet() {
+    let isMatch = firstCard.dataset.id === secondCard.dataset.id;
+    if (isMatch){
+            disableCards()
+        } else{
+            unflipCards();
+        }
+        
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+
+    }, timeBeforeUnflip);
+}
+
+function resetBoard() {
+    flippedCard = false;
+    lockCards = false;
+    firstCard = null;
+    secondCard = null;
 }
 
 setTimeout(function() {
-    $('.game-card').toggleClass('flip');
-}, timeRembemer);
+    $('.game-card').removeClass('flip');
+
+    resetBoard();
+
+}, timeForRembemer);
 
 /*divCards.forEach(card => card.addEventListener('DOMContentLoaded', flipCard));*/
-divCards.forEach(card => card.addEventListener('click', flipCard));
+htmlCards.forEach(card => card.addEventListener('click', flipCard));
